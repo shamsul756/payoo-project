de712a65d83d1b8a1994d19a23d7dd62ad345daa
shamsul765 <webdevlopershamsul@gmail.com>
2026-02-20 23:16:07 +0600

    complete functional addmoney.js section:

--- script/addmoney.js
+++ script/addmoney.js
@@ -1,6 +1,5 @@
-
-document.getElementById("add-money-btn").addEventListener("click", function () {
-    // 1.bank account get 
+ document.getElementById("add-money-btn").addEventListener("click",function(){
+        // 1.bank account get 
     const bankAccount = getValueFromInput("add-money-bank");
     if (bankAccount === "Select a Bank") {
         alert("please select a bank");
@@ -11,7 +10,7 @@ document.getElementById("add-money-btn").addEventListener("click", function () {
     const accountNumber = getValueFromInput("add-money-number");
     if (accountNumber.length != 11) {
         alert("invalid account number")
-    }
+    };
     // 3.get amount 
     const amount = getValueFromInput("add-money-amount");
     const newBalance = getBalance() + Number(amount);
@@ -26,12 +25,33 @@ document.getElementById("add-money-btn").addEventListener("click", function () {
              from ${bankAccount} 
              at ${new Date}`);
         setBalance(newBalance);
-    }
+
+        // 1.catch history container
+        const history = document.getElementById("history-container")
+        // 2. create a new div
+        const newHistory = document.createElement("div");
+         // 3.add new div inner html
+     newHistory.innerHTML = ` 
+            <div class="transaction-card p-5 bg-base-200">
+                <h4 class="font-bold">Add Money Success</h4>
+                <p>From: ${bankAccount} (${accountNumber})</p>
+                <p class="text-sm text-gray-500">${new Date().toLocaleString()}</p>
+                <p class="text-green-600 font-bold">+$${amount}</p>
+            </div>
+        `;
+          
+  
+
+        // 4. append new div in history container
+        history.append(newHistory);
+  }
     else {
-        alert("invalid pin ");
+        alert("invalid pin");
         return;
     }
-})
+    })
+    
+
 
 
 
